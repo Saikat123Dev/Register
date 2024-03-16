85cfbf5a8d3304022c5c3308d573aba427a9d7ae
Saikat123Dev <berasaikat729@gmail.com>
2024-03-17 02:14:50 +0530

    Student

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
 
-import './App.css'
-import Component from './upload';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
+
+import Dashboard from './scenes/dashboard';
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Team from './scenes/team';
+import Contacts from './scenes/contacts';
+import Attendence from './scenes/Attendence';
+import Form from './scenes/form';
+import Calendar from './scenes/calendar/';
+import Component from "./upload"
+
 
 function App() {
-  
+  const [theme, colorMode] = useMode();
+
   return (
-    <>
-     <Component/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Component/>}/>
+      </Routes>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <Sidebar />
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path="/Dash" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/attendence" element={<Attendence />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
